0559e2e6a0942f37f9d957a85e5280189edf0b3b
Joey Cho <cjy12182000@naver.com>
2019-01-23 11:23:53 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -11,7 +11,12 @@ class PigLatinizer
       @piglatinized = @phrase[1..-1]+@phrase[0]+"ay"
     elsif @phrase=~ /\A[^aeiou]/ && @phrase[1..-1] =~ /\A[^aeiou]/ && @phrase[2..-1] =~ /\A[aeiou]/
       @piglatinized = @phrase[2..-1]+@phrase[0..1]+"ay"
+    elsif @phrase=~ /\A[^aeiou]/ && @phrase[1..-1] =~ /\A[^aeiou]/ && @phrase[2..-1] =~ /\A[^aeiou]/ && @phrase[3..-1] =~ /\A[aeiou]/
+      @piglatinized = @phrase[3..-1]+@phrase[0..2]+"ay"
+    else
+      @piglatinized = @phrase + "ay"
     end
+    @piglatinized
   end
       
 
