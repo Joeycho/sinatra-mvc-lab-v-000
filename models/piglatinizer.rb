2014b5d75d6363af8a6fad907a83b0d8ad413489
Joey Cho <cjy12182000@naver.com>
2019-01-23 11:35:43 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -11,14 +11,15 @@ class PigLatinizer
     @piglatinized = ""
     
     words.each do |e|
-    if e=~ /\A[^aeiou]/ && e[1..-1] =~ /\A[aeiou]/
-      @piglatinized += e[1..-1]+e[0]+"ay"+" "
-    elsif e=~ /\A[^aeiou]/ && e[1..-1] =~ /\A[^aeiou]/ && e[2..-1] =~ /\A[aeiou]/
-      @piglatinized += e[2..-1]+e[0..1]+"ay"+" "
-    elsif e=~ /\A[^aeiou]/ && e[1..-1] =~ /\A[^aeiou]/ && e[2..-1] =~ /\A[^aeiou]/ && e[3..-1] =~ /\A[aeiou]/
-      @piglatinized += e[3..-1]+e[0..2]+"ay"+" "
-    else
-      @piglatinized = e + "way"
+      if e=~ /\A[^aeiou]/ && e[1..-1] =~ /\A[aeiou]/
+        @piglatinized += e[1..-1]+e[0]+"ay"+" "
+      elsif e=~ /\A[^aeiou]/ && e[1..-1] =~ /\A[^aeiou]/ && e[2..-1] =~ /\A[aeiou]/
+        @piglatinized += e[2..-1]+e[0..1]+"ay"+" "
+      elsif e=~ /\A[^aeiou]/ && e[1..-1] =~ /\A[^aeiou]/ && e[2..-1] =~ /\A[^aeiou]/ && e[3..-1] =~ /\A[aeiou]/
+        @piglatinized += e[3..-1]+e[0..2]+"ay"+" "
+      else
+        @piglatinized = e + "way"
+      end
     end
     @piglatinized
   end
