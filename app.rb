5305ad5618abd8cb45f1f68be3b6c23223e2790b
Joey Cho <cjy12182000@naver.com>
2019-01-23 11:56:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,8 +7,7 @@ class App < Sinatra::Base
   end
   
   post '/piglatinize' do
-    @text = 
-    PigLatinizer.new(params[:user_phrase])
+    @text = PigLatinizer.new(params[:user_phrase])
     erb :result
   end
   
