8ae7898a17ae38c000133a4bba73e275539f95fe
Joey Cho <cjy12182000@naver.com>
2019-01-23 11:41:45 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -11,11 +11,11 @@ class PigLatinizer
     @piglatinized = ""
     
     words.each do |e|
-      if e=~ /\A[^aeiou]/ && e[1..-1] =~ /\A[aeiou]/
+      if e=~ /\A[^aeiouAEIOU]/ && e[1..-1] =~ /\A[aeiouAEIOU]/
         @piglatinized += e[1..-1]+e[0]+"ay"+" "
-      elsif e=~ /\A[^aeiou]/ && e[1..-1] =~ /\A[^aeiou]/ && e[2..-1] =~ /\A[aeiou]/
+      elsif e=~ /\A[^aeiouAEIOU]/ && e[1..-1] =~ /\A[^aeiouAEIOU]/ && e[2..-1] =~ /\A[aeiouAEIOU]/
         @piglatinized += e[2..-1]+e[0..1]+"ay"+" "
-      elsif e=~ /\A[^aeiou]/ && e[1..-1] =~ /\A[^aeiou]/ && e[2..-1] =~ /\A[^aeiou]/ && e[3..-1] =~ /\A[aeiou]/
+      elsif e=~ /\A[^aeiouAEIOU]/ && e[1..-1] =~ /\A[^aeiouAEIOU]/ && e[2..-1] =~ /\A[^aeiouAEIOU]/ && e[3..-1] =~ /\A[aeiouAEIOU]/
         @piglatinized += e[3..-1]+e[0..2]+"ay"+" "
       else
         @piglatinized += e + "way"+" "
