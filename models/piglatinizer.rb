8e72e17e37c1a537a755120f8edf524b49f859b4
Joey Cho <cjy12182000@naver.com>
2019-01-23 11:21:44 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -7,13 +7,11 @@ class PigLatinizer
 
 
   def piglatinize
-    if @phrase =~ (/\A[aeiou]/i)
-    @piglatinized = @phrase + 'ay'
-    elsif @phrase =~ (/\A[^aeiou]/i)
-      match = /\A[^aeiou]/i.match(@phrase)
-      @piglatinized = match.post_match + match.to_s + 'ay'
+    if @phrase=~ /\A[^aeiou]/ && @phrase[1..-1] =~ /\A[aeiou]/
+      @piglatinized = @phrase[1..-1]+@phrase[0]+"ay"
+    elsif @phrase=~ /\A[^aeiou]/ && @phrase[1..-1] =~ /\A[^aeiou]/ && @phrase[2..-1] =~ /\A[aeiou]/
+      @piglatinized = @phrase[2..-1]+@phrase[0..1]+"ay"
     end
-  @piglatinized
   end
       
 
