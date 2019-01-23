032dd2595b17a06287f03b512c18d1d955f0c23a
Joey Cho <cjy12182000@naver.com>
2019-01-23 11:18:23 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -7,13 +7,13 @@ class PigLatinizer
 
 
   def piglatinize
-    if word =~ (/\A[aeiou]/i)
-    word = word + 'ay'
-    elsif word =~ (/\A[^aeiou]/i)
-      match = /\A[^aeiou]/i.match(word)
-      word = match.post_match + match.to_s + 'ay'
+    if @phrase =~ (/\A[aeiou]/i)
+    @piglatinized = @phrase + 'ay'
+    elsif @phrase =~ (/\A[^aeiou]/i)
+      match = /\A[^aeiou]/i.match(@phrase)
+      @piglatinized = match.post_match + match.to_s + 'ay'
     end
-  word
+  @piglatinized
   end
       
 
